a962ab6145db38d2009b2a7dc15598ef1ec8564e
ravindra-mhaske <ravindra.mhaske@softdel.com>
2020-06-16 16:06:30 +0530

    Updated app version to 0.0.5.1

--- modbus-master/Modbus-App/src/Main.cpp
+++ modbus-master/Modbus-App/src/Main.cpp
@@ -36,7 +36,7 @@ std::mutex mtx;
 std::condition_variable cv;
 bool g_stop = false;
 
-#define APP_VERSION "0.0.5.0"
+#define APP_VERSION "0.0.5.1"
 #define TIMER_TICK_FREQ 1000 // in microseconds
 
 /// flag to stop all running threads
