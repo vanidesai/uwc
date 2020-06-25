27423df347b8e91d11560dc85b019c9ab3fdf89a
ravindra-mhaske <ravindra.mhaske@softdel.com>
2020-06-25 17:52:48 +0530

    Added missing copyright

--- scada-rtu/SCADA-RTU-APP/Debug/src/subdir.mk
+++ scada-rtu/SCADA-RTU-APP/Debug/src/subdir.mk
@@ -1,6 +1,13 @@
 ################################################################################
-# Automatically-generated file. Do not edit!
+# The source code contained or described herein and all documents related to
+# the source code ("Material") are owned by Intel Corporation. Title to the
+# Material remains with Intel Corporation.
+#
+# No license under any patent, copyright, trade secret or other intellectual
+# property right is granted to or conferred upon you by disclosure or delivery of
+# the Materials, either expressly, by implication, inducement, estoppel or otherwise.
 ################################################################################
+
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
